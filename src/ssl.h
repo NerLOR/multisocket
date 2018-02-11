e3bc188e98244314585cdd0d4076a81de2bc95ef
Lorenz Stechauner <lorenz.stechauner@htl.rennweg.at>
2018-02-11 10:54:37 +0100

    TLSv1.2

--- src/ssl.h
+++ src/ssl.h
@@ -101,11 +101,11 @@ static int multi_tcp_encrypt(lua_State *L) {
 
     const SSL_METHOD *method;
     if (sock->servers) {
-        method = TLSv1_server_method();
+        method = TLSv1_2_server_method();
     } else if (sock->clients) {
-        method = TLSv1_client_method();
+        method = TLSv1_2_client_method();
     } else {
-        method = TLSv1_method();
+        method = TLSv1_2_method();
     }
 
     SSL_CTX *ctx = SSL_CTX_new(method);
